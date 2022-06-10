5d3c65ed6db81dee65b8e3f8668c29483a69584a
Prashant Priyadarshi <prashantpriyadarshi29@gmail.com>
2022-06-10 23:26:17 +0530

    Updates

---
+++ precache-manifest.b7a216c49c7fa5da1abae445a9693a57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0f707abc3f69227ff94",
+    "url": "/textUtils/static/css/main.56cdda4f.chunk.css"
+  },
+  {
+    "revision": "c0f707abc3f69227ff94",
+    "url": "/textUtils/static/js/main.c0f707ab.chunk.js"
+  },
+  {
+    "revision": "a3c0bc647fa7f281b58f",
+    "url": "/textUtils/static/js/1.a3c0bc64.chunk.js"
+  },
+  {
+    "revision": "b031524d289a8dd032e2",
+    "url": "/textUtils/static/js/runtime~main.b031524d.js"
+  },
+  {
+    "revision": "2cfe468b56b0bd9b8b5ad55958e77a33",
+    "url": "/textUtils/index.html"
+  }
+];
