8fc5af0028bab7446ebc781c138f3fb7baecd026
Prashant Priyadarshi <prashantpriyadarshi29@gmail.com>
2022-06-11 11:59:26 +0530

    fix 1.0

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,9 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          {/* <a className="navbar-brand" href="/">
             {props.title}
-          </a>
+          </a> */}
           {/* <Link className="navbar-brand" to="/">
             {props.title}
           </Link> */}
